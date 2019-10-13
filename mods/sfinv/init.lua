ca913c5c73c80da149d2d0a262a4f71d3fe2e0e9
maikerumine <michaellorenger@yahoo.com>
2019-10-13 18:01:33 -0400

    add unified inv to help fix craft guide

--- mods/sfinv/init.lua
+++ mods/sfinv/init.lua
@@ -1,12 +1,7 @@
--- sfinv/init.lua
-
 dofile(minetest.get_modpath("sfinv") .. "/api.lua")
 
--- Load support for MT game translation.
-local S = minetest.get_translator("sfinv")
-
 sfinv.register_page("sfinv:crafting", {
-	title = S("Crafting"),
+	title = "Crafting",
 	get = function(self, player, context)
 		return sfinv.make_formspec(player, context, [[
 				list[current_player;craft;1.75,0.5;3,3;]
